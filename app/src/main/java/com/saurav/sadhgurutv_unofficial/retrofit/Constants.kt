275f101b1c80218dcd4764adb13bf5218d8d1498
SauravC <sauravchandra94@gmail.com>
2023-05-21 17:30:44 +0530

    cleaning

--- app/src/main/java/com/saurav/sadhgurutv_unofficial/retrofit/Constants.kt
+++ app/src/main/java/com/saurav/sadhgurutv_unofficial/retrofit/Constants.kt
@@ -1,11 +1,13 @@
 package com.saurav.sadhgurutv_unofficial.retrofit
 
+import com.saurav.sadhgurutv_unofficial.BuildConfig
+
 object Constants {
     const val MOVIE = "Movie"
     const val VERSION = 1 // APP VERSION COMPARED IN API FROM HERE
-    const val BASE_URL = "https://raw.githubusercontent.com/TheSauravChandra/SG_Unofficial/main/"
     const val UPDATE = "UPDATE APP*"
-    const val UPDATE_CTA = "UPDATE AVAILABLE, PLS DOWNLOAD NEW VERSION .APK FROM http://sauravc.dx.am/sgtv"
-    const val SG_THUMB = "https://raw.githubusercontent.com/TheSauravChandra/SG_Unofficial/main/1664482659-explosive-guided-meditations-1.webp"
-    const val UPGRADE_THUMB = "https://raw.githubusercontent.com/TheSauravChandra/SG_Unofficial/main/update.png"
+    const val UPDATE_CTA =
+        "UPDATE AVAILABLE, PLS DOWNLOAD NEW VERSION .APK FROM http://sauravc.dx.am/sgtv"
+    const val SG_THUMB = BuildConfig.BASE_URL + "/1664482659-explosive-guided-meditations-1.webp"
+    const val UPGRADE_THUMB = BuildConfig.BASE_URL + "/update.png"
 }
--- app/src/main/java/com/saurav/sadhgurutv_unofficial/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/saurav/sadhgurutv_unofficial/retrofit/RetrofitClient.kt
@@ -1,6 +1,6 @@
 package com.saurav.sadhgurutv_unofficial.retrofit
 
-import com.saurav.sadhgurutv_unofficial.retrofit.Constants.BASE_URL
+import com.saurav.sadhgurutv_unofficial.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,7 +8,7 @@ object RetrofitClient {
     private var _apiService: ApiInterface? = null
 
     private fun getRetrofit() =
-        Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
+        Retrofit.Builder().baseUrl(BuildConfig.BASE_URL).addConverterFactory(GsonConverterFactory.create())
             .build()
 
     fun getApiService(): ApiInterface {
